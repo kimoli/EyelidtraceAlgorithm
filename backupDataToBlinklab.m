bef05d913bd9c2c62e2af0be1ed16d005e6e3be3
Olivia Kim <kim.olivia.a@gmail.com>
2019-01-07 11:41:03 -0600

    added a file for backing processed data up to server
    backing processed data up to server from passport drive

---
+++ backupDataToBlinklab.m
@@ -0,0 +1,106 @@
+% backup data processing output files to the C drive on OREK before plugging the
+% external drive back into ALBUS (concerned about the transition from
+% Windows 10 to Windows 7 corrupting the hard drive again)
+
+% also check for differences between the trials and newTrials structures'
+% information about the trial parameters
+
+close all
+clear all
+
+baseDir = 'G:\data\behavior';
+cd(baseDir)
+folders = dir;
+mice = folders(3:end);
+
+backupDir = 'L:\users\okim\behavior';
+
+for m = 1:length(mice)
+    
+    mouseFolder = strcat(baseDir, '\', mice(m,1).name);
+    cd(mouseFolder)
+    
+    folders = dir;
+    days = folders(3:end);
+    
+    for d = 1:length(days)
+        
+        % sometimes there is a condparams file or extra folder in the
+        % directory that does not have daydata in it
+        try
+            dayFolder = strcat(mouseFolder, '\', days(d,1).name);
+            cd(dayFolder)
+            gotIntoDay = 1;
+        catch ME
+            gotIntoDay = 0;
+        end
+        
+        if gotIntoDay
+            noCRTrialFilename = strcat(mice(m,1).name, '_', ...
+                days(d,1).name, '_noCRTrials.mat');
+            
+            if exist('newTrialdata.mat', 'file') == 2
+                load('newTrialdata.mat')
+                load('rodInfo.mat')
+                saveTrials = 1;
+                
+                % double check to make sure that newTrialdata is consistent
+                % with the old trialdata
+                oldTrials = load('trialdata.mat');
+                if sum(oldTrials.trials.c_csnum-trials.c_csnum) ||...
+                        sum(oldTrials.trials.c_usnum-trials.c_usnum) ||...
+                        sum(oldTrials.trials.c_csdur-trials.c_csdur) ||...
+                        sum(oldTrials.trials.c_usdur-trials.c_usdur) ||...
+                        sum(oldTrials.trials.laser.dur-trials.laser.dur) ||...
+                        sum(oldTrials.trials.laser.delay-trials.laser.delay) ||...
+                        sum(oldTrials.trials.laser.amp-trials.laser.amp) ||...
+                        sum(oldTrials.trials.laser.freq-trials.laser.freq) ||...
+                        sum(oldTrials.trials.laser.pulsewidth-trials.laser.pulsewidth)
+                    disp('!!DISCREPANCY BETWEEN TRIALDATA AND NEWTRIALDATA!!')
+                    pause
+                end
+                
+            else
+                saveTrials = 0;
+            end
+            
+            if exist(noCRTrialFilename, 'file')==2
+                load(noCRTrialFilename)
+                saveNoCRList = 1;
+            else
+                saveNoCRList = 0;
+            end
+            
+            
+            if saveTrials==1 || saveNoCRList == 1
+                %pause
+                cd(backupDir)
+                
+                bckupMouseFolder = strcat(backupDir,'\',mice(m,1).name);
+                if exist(mice(m,1).name, 'dir')==0
+                    mkdir(mice(m,1).name)
+                end
+                cd(bckupMouseFolder)
+                
+                bckupDayFolder = strcat(bckupMouseFolder, '\', days(d,1).name);
+                if exist(days(d,1).name, 'dir')==0
+                    mkdir(days(d,1).name)
+                end
+                cd(bckupDayFolder)
+                
+                if saveTrials
+                    save('newTrialdata.mat', 'trials')
+                    save('rodInfo.mat', 'rodMasks', 'rodPatches', 'rodEffective', 'calibAtRODSetting', 'threshAtRODSetting')
+                end
+                
+                if saveNoCRList
+                    save(noCRTrialFilename, 'markedTrials')
+                end
+            end
+            
+            cd(mouseFolder)
+        end
+        
+    end
+    
+end
